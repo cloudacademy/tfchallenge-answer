68ecee78f637eb4e2860683eaaee0b0e3a7c4c7e
Linux User <luke.orellana@cloudacademy.com>
2020-12-04 18:25:21 +0000

    Add Answer

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.7"
+    }
+  }
+  backend "s3" {
+    bucket = "RENAMEME!"
+    key    = "calabs/production/us-west-2/rslab/terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "TABLENAME!"
+    encrypt        = true
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = var.subnet
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "${var.servername}subnet"
+  }
+}
+
+resource "aws_instance" "server" {
+    ami           = lookup(var.ami_ids, var.os_type, null)
+    instance_type = var.instance_size
+    vpc_security_group_ids = [aws_vpc.example.default_security_group_id]
+    subnet_id = aws_subnet.example.id               
+    
+    tags = {
+        Name = var.servername
+    }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+servername = "calabvm"
+subnet = "10.0.1.0/24"
+os_type = "linux"
+disk = {
+    delete_on_termination = false
+    encrypted = true
+    volume_size = "15"
+    volume_type = "standard"
+}
